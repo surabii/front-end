429748d9e022a60db8afddc449a4147a598396a8
surabiha <smuraleedaran@q30design.com>
2026-04-09 11:39:36 -0400

    publish to gh-pages

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+export default defineConfig({
+  base: '/front-end/',
+})
+
