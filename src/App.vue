12d60d11aec11c0f6d5f16b5d57afc7d833e33d3
surabiha <smuraleedaran@q30design.com>
2026-04-09 11:15:47 -0400

    adding pages

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Hello from vue</h1>
+  <router-view></router-view>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
